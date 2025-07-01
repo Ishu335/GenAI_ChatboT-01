32cb0450d6c9cea871711860ca8d451483a72ee5
Dexter◥▶_◀◤ <335ishwar@gmail.com>
2025-07-01 13:16:02 +0530

    add Components and Stypling Sidebar

---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Main.css'
+const Main = () => {
+  return (
+    <div className='main'>
+    <div className="nav">
+        <p>Gimini</p>
+        <img src='{assets.user_icon}' alt=""/>
+    </div>
+    </div>
+  )
+}
+
+export default Main
