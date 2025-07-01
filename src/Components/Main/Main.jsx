530c8ae5d7d3c2a7a7c063b7d75daa91b0fdfc5a
Dexter◥▶_◀◤ <335ishwar@gmail.com>
2025-07-01 14:20:08 +0530

    main frame is Completed

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -1,11 +1,49 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
 import './Main.css'
 const Main = () => {
   return (
     <div className='main'>
     <div className="nav">
         <p>Gimini</p>
-        <img src='{assets.user_icon}' alt=""/>
+        <img src={assets.user_icon} alt=""/>
+    </div>
+    <div className="main-container">
+        <div className="greet">
+            <p><span>Hello, Dev.</span></p>
+            <p>How can I help you today</p>
+        </div>
+        <div className="cards">
+            <div className="card">
+                <p>Suggest beautiful places to see on an upcoming road trip</p>
+                <img src={assets.compass_icon} alt="" />
+            </div>
+            <div className="card">
+                <p>Briefly summerize this concept: urban planning</p>
+                <img src={assets.bulb_icon} alt="" />
+            </div>
+            <div className="card">
+                <p>Brainstrom team bonding activites for our work retreat</p>
+                <img src={assets.message_icon} alt="" />
+            </div>
+            <div className="card">
+                <p>Improve the readability of the following code</p>
+                <img src={assets.code_icon} alt="" />
+            </div>
+        </div>
+        <div className="main-botton">
+            <div className="search-box">
+                <input type="text" placeholder='Enter the Prompt here' />
+                <div>
+                    <img src={assets.gallery_icon} alt="" srcset="" />
+                    <img src={assets.mic_icon} alt="" srcset="" />
+                    <img src={assets.send_icon} alt="" srcset="" />
+                </div>
+            </div>
+            <p className='botton-info'>
+                Gemini may display inaccurate info, including about people,so double-click its responses. Your Privacy and Gemini Apps
+            </p>
+        </div>
     </div>
     </div>
   )
